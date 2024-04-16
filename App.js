de275c0d2ae111092faf446bbbf2257e1c5cc62b
Jaskaran Nanda <sahibsnanda@gmail.com>
2024-04-16 13:34:42 +0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "h1 tag 1"),
+    React.createElement("h2", {}, "h2 tag 1"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "h1 tag 2"),
+    React.createElement("h2", {}, "h2 tag 2"),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
