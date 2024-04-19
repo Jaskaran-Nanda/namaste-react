b75d0281a5c81b356d536b0a49713476e38e002f
Jaskaran-Nanda <sahibsnanda@gmail.com>
2024-04-19 19:40:06 +0400

    completed Episode 3 of Namaste React

--- Ep1,2.5/App.js
+++ Ep1,2.5/App.js
@@ -1,15 +1,42 @@
+//* Importing React into our App
 import React from "react";
 import ReactDOM from "react-dom/client";
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "h1 tag 1"),
-    React.createElement("h2", {}, "h2 tag 1"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "h1 tag 2"),
-    React.createElement("h2", {}, "h2 tag 2"),
-  ]),
-]);
 
+// React Element => ReactElement - JS Object => HTML Element (render)
+// JSX - HTML-like or XML-like syntax but not the same (transpiled before it reaches javascript engine) - Parcel - Babel
+// JSX =>  Babel transpiles it to React.createElement ==> ReactElement - JS Object => HTML Element (render)
+
+//React Element is just a normal variable so can be used inside {} as a javascript code
+
+const element = <span> !This is an element! </span>;
+const elem = (
+  <h2 className="xyzkans" tabIndex="5">
+    Namaste
+    {element}
+    React using JSX
+  </h2>
+);
+//Functional Component
+
+const TitleComponent = () => (
+  <h1 className="root" tabIndex="5">
+    Namaste React using JSX 🚀
+  </h1>
+);
+// *Component Composition
+const number = 1000;
+const HeadingComponent = () => (
+  <div id="container">
+    <TitleComponent />
+    <TitleComponent></TitleComponent>
+    {TitleComponent()}
+    {elem}
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(<HeadingComponent />);
+// Three ways of calling a functional component are
+// * [ <TitleComponent /> | <TitleComponent></TitleComponent> | {TitleComponent()} ]
+// as a function and can be called in {} as it is just Java Script code
